7377d2a2f6da3d6fc17a64a70cc55b5c94fc6e2b
DisketteFox <diskettefox@outlook.com>
2026-03-03 19:22:56 +0100

    Order by extension now works (In Linux)

--- MenuOrganize.py
+++ MenuOrganize.py
@@ -11,6 +11,9 @@ class MenuOrganize:
         self.root = tk.Tk()
         self.root.geometry("400x250")
 
+        # self.path = "C:\\Users\\Usuario\\Downloads\\"
+        self.path = "/home/miles/Documents/example/"
+
         tk.Label(self.root, text="¿Cómo quieres organizar los archivos?", font=("Arial", 16)).pack(pady=20)
         opciones = [
             "Por extensión (pdf, png, etc.)",
@@ -21,7 +24,7 @@ class MenuOrganize:
         self.combo = ttk.Combobox(self.root, values=opciones, state="readonly", width=30)
         self.combo.pack(pady=10)
 
-        self.button = tk.Button(self.root, text="Aplicar", command=self.mostrar_seleccion)
+        self.button = tk.Button(self.root, text="Aplicar", command=self.order)
         self.button.pack(pady=10)
 
         self.button_exit = tk.Button(self.root, text="Volver", font=("Arial", 12), command=self.root.destroy)
@@ -29,32 +32,26 @@ class MenuOrganize:
 
         self.root.mainloop()
 
-    def divideXtipos(self):
-        self.borraFrames()
-        ruta = "C:\\Users\\Usuario\\Downloads\\"
-        busquedaRuta = os.listdir(ruta)
-        respueta=tk.LabelFrame(self.root)
-        for archivo in busquedaRuta:
-            if archivo != "desktop.ini":
-                tipoArchivo = archivo.split(".")[len(archivo.split(".")) - 1]
-                src = os.path.join(ruta, archivo)  # origen
-                print("ruta de inicio ", src)
-                dst = os.path.join(os.getcwd() + "\\" + f"archivos de tipo {tipoArchivo}")  # destino
-                print("ruta de destino ", dst)
-                if not os.path.exists(ruta + archivo + tipoArchivo):
-                    try:
-                        # esta vaina crea directamente en el python toca que se cree en el lugar especificado.
-                        os.mkdir(f"archivos de tipo {tipoArchivo}")
-                    except Exception:
-                        print()
-                shutil.copy(src, dst)
-        tk.Label(respueta, text="Se han distribuidos los archivos de manera exitosa.", font=("Arial", 16)).pack(pady=20)
-        respueta.pack()
+    def order_by_extension(self):
+        files = os.listdir(self.path)
+
+        for file in files:
+            if file != "desktop.ini" and not os.path.isdir(self.path + file):
+                file_extension = file.split(".")[len(file.split(".")) - 1]
+
+                # Create folder if not exists
+                if not os.path.exists(self.path + file_extension):
+                    os.mkdir(self.path + file_extension)
+
+                src = self.path + file
+                dst = self.path + file_extension + "/" + file
+
+                shutil.move(src, dst)
+        ms.showinfo("Atención", "Se han distribuidos los archivos de manera exitosa")
 
     def ordenAlfabetico(self):
         self.borraFrames()
-        ruta = "C:\\Users\\User_yo\\Downloads\\"
-        busqueda_ruta = os.listdir(ruta)
+        busqueda_ruta = os.listdir(self.path)
         respuesta=tk.LabelFrame(self.root)
         for archivo in busqueda_ruta:
             if archivo != "desktop.ini":
@@ -63,11 +60,10 @@ class MenuOrganize:
 
     def ordenXFecha(self):
         self.borraFrames()
-        ruta = "C:\\Users\\User_yo\\Downloads\\"
-        busqueda_ruta = os.listdir(ruta)
+        busqueda_ruta = os.listdir(self.path)
         respuesta=tk.LabelFrame(self.root)
         # bucle con un if que agrega fecha y el nombre del archivo, medio volado andava creando esto y creativo
-        lista_fechas=[datetime.datetime.fromtimestamp(os.path.getctime(ruta + archivo)).strftime('%Y/%m/%d %H:%M')+" "+archivo for archivo in busqueda_ruta if archivo!="desktop.ini"]
+        lista_fechas=[datetime.datetime.fromtimestamp(os.path.getctime(self.path + archivo)).strftime('%Y/%m/%d %H:%M')+" "+archivo for archivo in busqueda_ruta if archivo!="desktop.ini"]
         lista_fechas.sort(reverse=True)
         for archivo in lista_fechas:
             tk.Label(respuesta, text=f"{archivo}", font=("Arial", 18)).pack(pady=10)
@@ -78,15 +74,14 @@ class MenuOrganize:
             if hijo.widgetName=="labelframe":
                 hijo.destroy()
 
-    def mostrar_seleccion(self):
-        seleccion = self.combo.get()
-        if seleccion == "":
+    def order(self):
+        option = self.combo.get()
+        if option == "":
             ms.showwarning("Atención", "Selecciona una opción")
             return
-
-        if seleccion == "Por extensión (pdf, png, etc.)":
-            self.divideXtipos()
-        if seleccion == "Por fecha":
+        if option == "Por extensión (pdf, png, etc.)":
+            self.order_by_extension()
+        if option == "Por fecha":
             self.ordenXFecha()
-        if seleccion == "Orden alfabético":
+        if option == "Orden alfabético":
             self.ordenAlfabetico()
